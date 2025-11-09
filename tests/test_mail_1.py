d32b25eca0a0021da9581501f05990688b19f681
Evg1971 <mosoloeve@gmail.com>
2025-11-09 21:54:09 +0300

    Добавление файла

---
+++ tests/test_mail_1.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture
+def set_up():
+    print('Пользователь зарегистрировался')
+
+
+def test_sending_mail_1(set_up):
+    print('Письмо отправлено')
+
+
+def test_sending_mail_2(set_up):
+    print('Письмо отправлено')
---
+++ tests/test_mail_2.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.fixture
+def set_up():
+    print('Пользователь зарегистрировался')
+
+def test_sending_mail_1():
+    print('Письмо отправлено')
+
+def test_sending_mail_2():
+    print('Письмо отправлено')
