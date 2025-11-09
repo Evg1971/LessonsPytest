403cdc7a1bb6867db9e714c7a7e6ccd2f02bd266
Evg1971 <mosoloeve@gmail.com>
2025-11-09 22:18:22 +0300

    ДЗ: Создание проекта и импортирование библиотеки Pytest

--- tests/test_mail_1.py
+++ tests/test_mail_1.py
@@ -5,10 +5,8 @@ import pytest
 def set_up():
     print('Пользователь зарегистрировался')
 
-
 def test_sending_mail_1(set_up):
     print('Письмо отправлено')
-
 
 def test_sending_mail_2(set_up):
     print('Письмо отправлено')
